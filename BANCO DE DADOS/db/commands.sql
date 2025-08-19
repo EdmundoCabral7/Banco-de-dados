46a47963da8487a052c3e8b2260c410615c96722
Laboratório 04 <lab04@acadunirn.lan>
2025-08-19 20:46:40 -0300

    banco de dados 1.9

--- BANCO DE DADOS/db/commands.sql
+++ BANCO DE DADOS/db/commands.sql
@@ -19,3 +19,16 @@ INSERT INTO cursos(id, nome, aulas) VALUES (1, 'HTML', 10);
 
 INSERT INTO cursos(id, nome, aulas) VALUES (2, 'CSS', 12);
 
+DELETE FROM "cursos";
+
+DELETE FROM "cursos" WHERE id = 0;
+
+SELECT "id", "nome" FROM "cursos";
+
+SELECT * FROM "cursos" LIMIT 2;
+
+SELECT * FROM "cursos" WHERE "id" = 2 AND "aulas" > 10;
+
+SELECT * FROM "cursos" WHERE "id" = 2 OR "aulas" > 10;
+
+UPDATE "cursos" SET "nome" = 'HTML e CSS', "aulas" = 20 WHERE "id" = 1;
