0aab1beacc78a2b69ab744245fb4156ec302d345
Laboratório 04 <lab04@acadunirn.lan>
2025-08-19 20:20:39 -0300

    banco de dados 1

---
+++ BANCO DE DADOS/db/commands.sql
@@ -0,0 +1,14 @@
+-- database: ./db.sqlite
+
+CREATE TABLE cursos (id, nome, aulas);
+
+DROP TABLE cursos;
+
+
+CREATE TABLE cursos (
+    id INTERGET NOT NULL,
+    nome TEXT NOT NULL,
+    aulas INTEGER
+);
+
+DROP TABLE curso;
